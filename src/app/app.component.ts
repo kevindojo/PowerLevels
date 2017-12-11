514fd2ff1a83b1f6d1efbbe4138a11a10ab42662
KevinPham <kevinpham15@gmail.com>
2017-12-11 13:41:30 -0800

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,26 @@ export class AppComponent {
 
   powerLevel: number;
   power;
+  message;
 
   calcPower(){
     this.power = this.powerLevel;
     console.log(this.powerLevel);
+  }
+
+  Powermessage(power){
+    if(power > '9000' && power <= 20001){
+      this.message = "It's over 9000!!!"
+    }
+    else if (power > '20000' && power <= 500001) {
+      this.message = "Superlative power!"
+    }
+    else if (power === '500000'){
+      this.message = "The Chosen One"
+    }
+    else{
+      this.message = "weak sauce";
+    }
   }
 
 
